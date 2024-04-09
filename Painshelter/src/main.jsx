62506f3c9236128c82f7271981219a027e74978d
Max <milk88084@gmail.com>
2024-04-09 20:26:57 +0800

    feat(router): configure routes and complete environment setup.

--- Painshelter/src/main.jsx
+++ Painshelter/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Member from "./pages/Member/index.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="member" element={<Member />} />
+      </Routes>
+    </BrowserRouter>
+
+    {/* <App /> */}
+  </React.StrictMode>
+);
