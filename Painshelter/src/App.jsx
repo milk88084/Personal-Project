b0de4fcddf38327c2be3928fc83e1cca7984c817
Max <milk88084@gmail.com>
2024-04-11 09:52:58 +0800

    feat: initialize base website structure.

--- Painshelter/src/App.jsx
+++ Painshelter/src/App.jsx
@@ -1,8 +1,21 @@
 import "./App.css";
 import { useNavigate } from "react-router-dom";
+import { useFormInput } from "./utils/hooks/useFormInput";
+import { useState } from "react";
 
 function App() {
   const navigate = useNavigate();
+  const nameProps = useFormInput("Max");
+  const emailProps = useFormInput("exmple.com");
+  const passwordProps = useFormInput("<PASSWORD>");
+  const [loginState, setLoginState] = useState(false);
+
+  const loginName = true;
+
+  const handleLogin = () => {
+    setLoginState(true);
+  };
+
   return (
     <>
       <div>標題</div>
@@ -11,6 +24,9 @@ function App() {
         <button className="bg-red-600 text-white">
           這裡是一個深夜模式按鈕
         </button>
+      </div>
+      <div>
+        <button className="bg-blue-600 text-white mt-3">登錄</button>
       </div>
       <div>
         <button
@@ -28,6 +44,23 @@ function App() {
       </div>
 
       <button className="bg-gray-600 text-white mt-3">心靈緊急按鈕</button>
+
+      {/* <div className="h-full w-full absolute top-0 right-0 border-gray-300 flex items-center mr-8 gap-5 bg-black bg-opacity-75">
+        <div className="h-96 w-7/12 flex items-center bg-yellow-700">
+          <div className="flex flex-col">
+            <label>
+              請輸入登錄信箱:
+              <input className="mt-0.5" {...emailProps} />
+            </label>
+            <label className="mt-6">
+              請輸入登錄密碼:
+              <input {...passwordProps} />
+            </label>
+            <button className="mt-6 bg-white">登入</button>
+            <button className="mt-6 bg-white">我要註冊</button>
+          </div>
+        </div>
+      </div> */}
     </>
   );
 }
