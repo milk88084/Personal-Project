5b28abe3fe7a4855305d442b455438a9969a6bf2
Max <milk88084@gmail.com>
2024-04-26 14:37:07 +0800

    feat(login): update layout design.

--- Painshelter/src/pages/Login/index.jsx
+++ Painshelter/src/pages/Login/index.jsx
@@ -8,14 +8,111 @@ import logoImg from "../../assets/img/logoImg.png";
 import logoTitle from "../../assets/img/logoTitle.png";
 import backgroundVideo from "../../assets/video/login.mp4";
 
-const Background = styled.video``;
+const Background = styled.div`
+  height: 100vh;
+  background-color: red;
+`;
+
+const BackgroundVideo = styled.video`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
 
 const BlackDiv = styled.div`
-  background-color: rgba(0, 0, 0, 0.5);
-  background-attachment: fixed;
-  min-height: 100vh;
-  width: 100vw;
-  margin: 0;
+  background-color: rgba(0, 0, 0, 0.9);
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 20;
+`;
+
+const LogoSection = styled.div`
+  position: absolute;
+  top: 15%;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 25;
+
+  img {
+    width: 15%;
+  }
+
+  img:first-of-type {
+    width: 7%; // 假設這是您只想應用到第一個 img 的特定樣式
+  }
+`;
+
+const MainSection = styled.div`
+  position: absolute;
+  top: 40%;
+  left: 38%;
+  z-index: 40;
+  margin: 0 auto;
+  background-color: rgba(255, 255, 255, 0.8);
+  width: 25%;
+  height: 50%;
+  border-radius: 20px;
+`;
+
+const FormSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const InputSection = styled.div`
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+
+  label {
+    margin-top: 30px;
+  }
+
+  input {
+    outline: none;
+    border: none;
+    border-bottom: 1px solid #202020;
+    background-color: rgba(255, 255, 255, 0);
+  }
+`;
+
+const ButtonSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 60%;
+  button {
+    display: block;
+    width: 100%;
+    background-color: #9ca3af;
+    padding: 5px;
+    border-radius: 10px;
+    font-weight: 400;
+    margin-top: 30px;
+    box-shadow: 0 10px 10px -8px rgba(0, 0, 0, 0.7);
+
+    &:hover,
+    &:focus {
+      background-color: #19242b;
+      color: white;
+    }
+  }
+
+  button:first-of-type {
+    background-color: #001a2a;
+    color: white;
+    &:hover,
+    &:focus {
+      background-color: #4c5e67;
+    }
+  }
 `;
 
 const Login = () => {
@@ -55,22 +152,24 @@ const Login = () => {
 
   return (
     <>
-      <Background ref={videoRef} src={backgroundVideo} loop autoPlay muted>
+      <Background>
+        <LogoSection>
+          <img src={logoImg} alt="logo" />
+          <img src={logoTitle} alt="brandName" />
+        </LogoSection>
+        <BackgroundVideo
+          src={backgroundVideo}
+          ref={videoRef}
+          loop
+          autoPlay
+          muted
+        ></BackgroundVideo>
         <BlackDiv></BlackDiv>
-      </Background>
-      <main>
-        <section>
-          <div>
-            <p> 登入頁面 </p>
-
-            <form>
-              <div className="mt-5">
-                <label
-                  htmlFor="email-address"
-                  className="block w-32 bg-blue-600 "
-                >
-                  請輸入信箱
-                </label>
+        <MainSection>
+          <form>
+            <FormSection>
+              <InputSection>
+                <label htmlFor="email-address">請輸入信箱</label>
                 <input
                   id="email-address"
                   name="email"
@@ -78,14 +177,11 @@ const Login = () => {
                   required
                   placeholder="Email address"
                   onChange={(e) => setEmail(e.target.value)}
-                  className=" border-2 border-black "
                 />
-              </div>
+              </InputSection>
 
-              <div className="mt-5">
-                <label htmlFor="password" className="block w-32 bg-blue-600 ">
-                  請輸入密碼
-                </label>
+              <InputSection>
+                <label htmlFor="password">請輸入密碼</label>
                 <input
                   id="password"
                   name="password"
@@ -93,25 +189,20 @@ const Login = () => {
                   required
                   placeholder="Password"
                   onChange={(e) => setPassword(e.target.value)}
-                  className=" border-2 border-black "
                 />
-              </div>
+              </InputSection>
 
-              <div className="mt-5">
-                <button
-                  className="mt-5 border-2 border-black"
-                  onClick={onLogin}
-                >
-                  登入
+              <ButtonSection>
+                <button onClick={onLogin}>登入</button>
+                <button>
+                  {" "}
+                  <NavLink to="/signup">註冊會員</NavLink>
                 </button>
-                <p className="text-smtext-center">
-                  尚未註冊? <NavLink to="/signup">註冊會員</NavLink>
-                </p>
-              </div>
-            </form>
-          </div>
-        </section>
-      </main>
+              </ButtonSection>
+            </FormSection>
+          </form>
+        </MainSection>
+      </Background>
     </>
   );
 };
