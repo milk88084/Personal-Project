7d56f1f80189e0a53fdcf82535f5bba45a216994
Max <milk88084@gmail.com>
2024-06-11 17:31:29 +0800

    (main page):modified modal.

--- Painshelter/src/components/ModalMain.jsx
+++ Painshelter/src/components/ModalMain.jsx
@@ -3,10 +3,10 @@ import { MainModal } from "@/utils/zustand.js";
 import modalpoem from "@/assets/img/modalpoem.jpg";
 import styled, { keyframes } from "styled-components";
 import Slider from "react-slick";
-import categories from "@/assets/video/categories.mp4";
-import highlight from "@/assets/video/highlight.mp4";
+import categories from "@/assets/gif/categories.gif";
+import highlight from "@/assets/gif/highlight.gif";
 import mainPageTypeGuide from "@/assets/img/mainPageTypeGuide.png";
-import painMap from "@/assets/video/painMap.mp4";
+import painMap from "@/assets/gif/painMap.gif";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -24,7 +24,6 @@ const Background = styled.div`
   height: 650px;
   position: fixed;
   display: flex;
-  justify-content: center;
   align-items: center;
   padding: 30px;
   flex-direction: column;
@@ -36,6 +35,14 @@ const Background = styled.div`
   bottom: 0;
   margin: auto auto;
   border-radius: 15px;
+  span {
+    width: 100%;
+    font-size: 20px;
+    font-weight: 900;
+    text-align: end;
+    cursor: pointer;
+  }
+
   h1 {
     font-size: 50px;
     font-weight: 600;
@@ -58,83 +65,53 @@ const Background = styled.div`
     margin-top: 30px;
   }
 
-  button {
-    padding: 0px;
-    width: 30px;
-    height: 30px;
-  }
-
-  .slick-next:before {
-    background-color: #363b44;
-    border-radius: 20px;
-  }
-
-  .slick-prev:before {
-    background-color: #363b44;
-    border-radius: 20px;
+  .slick-prev,
+  .slick-next {
+    visibility: hidden;
   }
 
   @media screen and (max-width: 1279px) {
     width: 400px;
-    height: 500px;
+    height: 400px;
     padding: 15px;
     border-radius: 12px;
+    align-items: center;
+
     h1 {
       font-size: 25px;
       margin-bottom: 0px;
       height: 50px;
     }
 
-    button {
-      padding: 7px;
-      border-radius: 10px;
-      margin: 20px;
-    }
-
     h2 {
       font-size: 14px;
       padding: 0;
-      margin-bottom: 25px;
     }
 
     div {
       width: 350px;
     }
 
-    video {
-      width: 300px;
-      text-align: center;
-      margin-left: 27.5px;
-    }
-
     img {
       width: 300px;
       margin-left: 27.5px;
     }
-
-    .slick-prev,
-    .slick-next {
-      top: 84px;
-    }
   }
 `;
 
-const VideoSection = styled.div`
+const ImgSection = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  video {
-    border-radius: 20px;
-  }
-
   img {
     border-radius: 20px;
+    cursor: pointer;
   }
 `;
 //#endregion
 
 export default function Modal() {
-  const { modal } = MainModal();
+  const { modal, closeModal } = MainModal();
   var settings = {
     dots: true,
     infinite: true,
@@ -147,29 +124,30 @@ export default function Modal() {
       <BackdropMainPage />
       {modal ? (
         <Background>
+          <span onClick={closeModal}>X</span>
           <h1>Tutorial</h1>
           <Slider {...settings}>
-            <VideoSection key={1}>
-              <video src={categories} loop autoPlay muted></video>
+            <ImgSection key={1}>
+              <img src={categories} alt={categories}></img>
               <h2>1.頁面右上方可點選按鈕功能前往功能頁面。</h2>
-            </VideoSection>
+            </ImgSection>
 
-            <VideoSection key={2}>
-              <video src={highlight} loop autoPlay muted></video>
+            <ImgSection key={2}>
+              <img src={highlight} alt={highlight}></img>
               <h2>2.文章精選可以觀看其他作者文章並進行互動。</h2>
-            </VideoSection>
+            </ImgSection>
 
-            <VideoSection key={3}>
-              <img src={mainPageTypeGuide}></img>
+            <ImgSection key={3}>
+              <img src={mainPageTypeGuide} alt={mainPageTypeGuide}></img>
               <h2>3.疼痛光譜蒐集使用者故事類型與關係人的統計數據。</h2>
-            </VideoSection>
+            </ImgSection>
 
-            <VideoSection key={4}>
-              <video src={painMap} loop autoPlay muted></video>
+            <ImgSection key={4}>
+              <img src={painMap} alt={painMap}></img>
               <h2>
                 4.玻璃瓶儲存了每個故事，使用者可以點選玻璃瓶，獲得一張專屬小卡，也可以前往該作者頁面或儲存圖片至您的裝置中。
               </h2>
-            </VideoSection>
+            </ImgSection>
           </Slider>
         </Background>
       ) : null}
